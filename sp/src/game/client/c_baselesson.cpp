2022af95191d816f211ef453d217b2a195508fb5
Jeremy Lorelli <jeremy.lorelli.1337@gmail.com>
2021-09-10 01:59:41 -0700

    fix: Fixed a bunch of bad preprocessor hacks

--- sp/src/game/client/c_baselesson.cpp
+++ sp/src/game/client/c_baselesson.cpp
@@ -15,7 +15,7 @@
 #include "ammodef.h"
 #include "vprof.h"
 #include "view.h"
-#include "vstdlib/ikeyvaluessystem.h"
+#include "vstdlib/IKeyValuesSystem.h"
 #ifdef MAPBASE
 #include "usermessages.h"
 #endif
@@ -666,7 +666,8 @@ void CIconLesson::UpdateInactive()
 			CUtlBuffer msg_data;
 			msg_data.PutChar( 1 );
 			msg_data.PutString( m_szHudHint.String() );
-			usermessages->DispatchUserMessage( usermessages->LookupUserMessage( "KeyHintText" ), bf_read( msg_data.Base(), msg_data.TellPut() ) );
+			auto read = bf_read( msg_data.Base(), msg_data.TellPut() );
+			usermessages->DispatchUserMessage( usermessages->LookupUserMessage( "KeyHintText" ), read );
 		}
 #endif
 
@@ -1039,40 +1040,40 @@ Vector CIconLesson::GetIconTargetPosition( C_BaseEntity *pIconTarget )
 
 #define LESSON_VARIABLE_INIT_SYMBOL( _varEnum, _varName, _varType ) g_n##_varEnum##Symbol = KeyValuesSystem()->GetSymbolForString( #_varEnum );
 
-#define LESSON_SCRIPT_STRING_ADD_TO_MAP( _varEnum, _varName, _varType ) g_NameToTypeMap.Insert( #_varEnum, LESSON_VARIABLE_##_varEnum## );
+#define LESSON_SCRIPT_STRING_ADD_TO_MAP( _varEnum, _varName, _varType ) g_NameToTypeMap.Insert( #_varEnum, LESSON_VARIABLE_##_varEnum );
 
 // Create enum value
-#define LESSON_VARIABLE_ENUM( _varEnum, _varName, _varType ) LESSON_VARIABLE_##_varEnum##,
+#define LESSON_VARIABLE_ENUM( _varEnum, _varName, _varType ) LESSON_VARIABLE_##_varEnum,
 
 // Init info call
-#define LESSON_VARIABLE_INIT_INFO_CALL( _varEnum, _varName, _varType ) g_pLessonVariableInfo[ LESSON_VARIABLE_##_varEnum## ].Init_##_varEnum##();
+#define LESSON_VARIABLE_INIT_INFO_CALL( _varEnum, _varName, _varType ) g_pLessonVariableInfo[ LESSON_VARIABLE_##_varEnum ].Init_##_varEnum ();
 
 // Init info
 #define LESSON_VARIABLE_INIT_INFO( _varEnum, _varName, _varType ) \
-	void Init_##_varEnum##() \
+	void Init_##_varEnum() \
 	{ \
-		iOffset = offsetof( CScriptedIconLesson, CScriptedIconLesson::##_varName## ); \
+		iOffset = offsetof( CScriptedIconLesson, CScriptedIconLesson::_varName ); \
 		varType = LessonParamTypeFromString( #_varType ); \
 	}
 
 #define LESSON_VARIABLE_INIT_INFO_BOOL( _varEnum, _varName, _varType ) \
-	void Init_##_varEnum##() \
+	void Init_##_varEnum() \
 	{ \
-		iOffset = offsetof( CScriptedIconLesson, CScriptedIconLesson::##_varName## ); \
+		iOffset = offsetof( CScriptedIconLesson, CScriptedIconLesson::_varName ); \
 		varType = FIELD_BOOLEAN; \
 	}
 
 #define LESSON_VARIABLE_INIT_INFO_EHANDLE( _varEnum, _varName, _varType ) \
-	void Init_##_varEnum##() \
+	void Init_##_varEnum() \
 	{ \
-		iOffset = offsetof( CScriptedIconLesson, CScriptedIconLesson::##_varName## ); \
+		iOffset = offsetof( CScriptedIconLesson, CScriptedIconLesson::_varName ); \
 		varType = FIELD_EHANDLE; \
 	}
 
 #define LESSON_VARIABLE_INIT_INFO_STRING( _varEnum, _varName, _varType ) \
-	void Init_##_varEnum##() \
+	void Init_##_varEnum() \
 	{ \
-		iOffset = offsetof( CScriptedIconLesson, CScriptedIconLesson::##_varName## ); \
+		iOffset = offsetof( CScriptedIconLesson, CScriptedIconLesson::_varName ); \
 		varType = FIELD_STRING; \
 	}
 
@@ -1090,19 +1091,19 @@ Vector CIconLesson::GetIconTargetPosition( C_BaseEntity *pIconTarget )
 	}
 
 // Wrapper for using this macro in the factory
-#define LESSON_SCRIPT_STRING_GENERAL( _varEnum, _varName, _varType ) LESSON_SCRIPT_STRING( LESSON_VARIABLE_##_varEnum##, #_varEnum )
+#define LESSON_SCRIPT_STRING_GENERAL( _varEnum, _varName, _varType ) LESSON_SCRIPT_STRING( LESSON_VARIABLE_##_varEnum, #_varEnum )
 
 // Process the element action on this variable 
 #define PROCESS_LESSON_ACTION( _varEnum, _varName, _varType ) \
-	case LESSON_VARIABLE_##_varEnum##:\
+	case LESSON_VARIABLE_##_varEnum:\
 		return ProcessElementAction( pLessonElement->iAction, pLessonElement->bNot, #_varName, _varName, &pLessonElement->szParam, eventParam_float );
 
 #define PROCESS_LESSON_ACTION_EHANDLE( _varEnum, _varName, _varType ) \
-	case LESSON_VARIABLE_##_varEnum##:\
+	case LESSON_VARIABLE_##_varEnum:\
 		return ProcessElementAction( pLessonElement->iAction, pLessonElement->bNot, #_varName, _varName, &pLessonElement->szParam, eventParam_float, eventParam_BaseEntity, eventParam_string );
 
 #define PROCESS_LESSON_ACTION_STRING( _varEnum, _varName, _varType ) \
-	case LESSON_VARIABLE_##_varEnum##:\
+	case LESSON_VARIABLE_##_varEnum:\
 		return ProcessElementAction( pLessonElement->iAction, pLessonElement->bNot, #_varName, &_varName, &pLessonElement->szParam, eventParam_string );
 
 // Init the variable from the script (or a convar)
